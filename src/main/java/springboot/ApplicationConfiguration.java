d8268106436e393d05cf609264956909939f6aa1
Oskar Henriksson <Oskar_h@live.se>
2020-02-27 10:01:22 +0100

    Configured db engine to use InnoDB instead of MyISAM, Configured CORS policy

--- src/main/java/springboot/ApplicationConfiguration.java
+++ src/main/java/springboot/ApplicationConfiguration.java
@@ -1,8 +1,18 @@
 package springboot;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ApplicationConfiguration {
-    //Empty Configuration for future possible expansion of the program
+@EnableWebMvc
+public class ApplicationConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST","PUT", "DELETE");
+    }
 }
